5e81c0095161ba4f9f72ecdd313aaf9135b74ef0
samejima-is-shark <shark.is.sugoi@gmail.com>
2025-08-08 09:58:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -203,6 +203,7 @@ function showDetail(story) {
 
   storyDetail.innerHTML = `
     <h2>${story.title}</h2>
+    <button id="closeDetailBtn"><i class="fa-solid fa-xmark"></i> ×</button>
     <p>${story.content.replace(/\n/g, "<br>")}</p>
     <div class="tags">${story.tags.map(tag => `<span class="tag">${tag}</span>`).join(" ")}</div>
     <div class="detail-buttons">
