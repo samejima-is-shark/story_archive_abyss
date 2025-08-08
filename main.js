c58b6cafc350c572a4ca5a094ce5224997c532b2
samejima-is-shark <shark.is.sugoi@gmail.com>
2025-08-08 10:01:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -202,8 +202,10 @@ function showDetail(story) {
   //window.scrollTo({ top: 0, behavior: "smooth" });
 
   storyDetail.innerHTML = `
-    <h2>${story.title}</h2>
-    <button id="closeDetailBtn"><i class="fa-solid fa-xmark"></i> ×</button>
+    <div class="detail-buttons">
+      <h2>${story.title}</h2>
+      <button id="closeDetailBtn"><i class="fa-solid fa-xmark"></i></button>
+    </div>
     <p>${story.content.replace(/\n/g, "<br>")}</p>
     <div class="tags">${story.tags.map(tag => `<span class="tag">${tag}</span>`).join(" ")}</div>
     <div class="detail-buttons">
